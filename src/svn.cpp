8a1468fda7259d2d476e142fc1a45c115be4ed75
Thiago Macieira <thiago@cassini.local.lan>
2007-12-24 15:22:07 -0200

    Don't give fast-import paths starting with a slash

--- src/svn.cpp
+++ src/svn.cpp
@@ -278,7 +278,11 @@ static int recursiveDumpDir(Repository::Transaction *txn, svn_fs_root_t *fs_root
 
         svn_fs_dirent_t *dirent = reinterpret_cast<svn_fs_dirent_t *>(value);
         QByteArray entryName = pathname + '/' + dirent->name;
-        QString entryFinalName = finalPathName + '/' + dirent->name;
+        QString entryFinalName;
+        if (finalPathName.isEmpty())
+            entryFinalName = dirent->name;
+        else
+            entryFinalName = finalPathName + '/' + dirent->name;
 
         if (dirent->kind == svn_node_dir) {
             if (recursiveDumpDir(txn, fs_root, entryName, entryFinalName, dirpool) == EXIT_FAILURE)
