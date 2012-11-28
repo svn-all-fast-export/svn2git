b5c3d3aa55621c44db439e4a73884e0d0bf9612c
Ulrich Spörlein <uqs@spoerlein.net>
2012-11-28 21:58:44 +0100

    Fix compilation, the last rebase/merge went wrong.
    Noticed by:	Andy Pilate

--- src/svn.cpp
+++ src/svn.cpp
@@ -896,7 +896,7 @@ int SvnRevision::recurse(const char *path, const svn_fs_path_change_t *change,
                                rev_from, changes, current, *match, matchRules, dirpool) == EXIT_FAILURE)
                 return EXIT_FAILURE;
         } else {
-            if (dirent->kind == svn_node_dir) {
+            if (i.value() == svn_node_dir) {
                 qDebug() << current << "rev" << revnum
                          << "did not match any rules; auto-recursing";
                 if (recurse(entry, change, entryFrom.isNull() ? 0 : entryFrom.constData(),
