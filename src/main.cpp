3d583c89965dd238e5ea4ffad721085bba4542a5
Torgny Nyblom <nyblom.torgny@gmail.com>
2019-05-09 16:31:43 +0200

    Revert "Revert Maintain annotated tags throughout incremental runs #60"
    This reverts commit 58378dcd8d758ccac2465547457dc586e2443fab.

--- src/main.cpp
+++ src/main.cpp
@@ -211,6 +211,7 @@ int main(int argc, char **argv)
         repositories.insert(rule.name, repo);
 
         int repo_next = repo->setupIncremental(cutoff);
+        repo->restoreAnnotatedTags();
         repo->restoreBranchNotes();
 
         /*
